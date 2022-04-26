71169b3987a9b3d71eb944018d7f93f2931722e7
Arun Kumar <arunkumar413@gmail.com>
2022-04-26 11:46:17 +0000

    adding name, title and skills

---
+++ my-resume/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>  </div>
+      <div>
+
+        <h1 className='name'>  Arun Kumar Kadari</h1>
+        <h4 className='title'>  Fullstack Developer</h4>
+
+
+
+      </div>
+
+      <div className='third-column skills'>
+        <div>
+          <h3> Nodejs </h3>
+        </div>
+        <div>
+          <h3> HTML </h3>
+        </div>
+        <div>
+          <h3> JavaScript </h3>
+        </div>
+
+        <div>
+          <h3> CSS </h3>
+        </div>
+
+        <div>
+          <h3> ExpressJs </h3>
+        </div>
+
+        <div>
+          <h3> Postgres </h3>
+        </div>
+        <div>
+          <h3> MySQL </h3>
+        </div>
+
+
+      </div>
+
+
+
+    </div >
+  );
+}
+
+export default App;
