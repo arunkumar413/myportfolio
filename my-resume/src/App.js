77be33cd8eb9069ec894d3d21c23e98df0d6c023
Arun Kumar <arunkumar413@gmail.com>
2022-04-26 17:16:11 +0000

    changig skills to second column

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -6,45 +6,65 @@ function App() {
     <div className="App">
       <div>  </div>
       <div>
-
         <h1 className='name'>  Arun Kumar Kadari</h1>
         <h4 className='title'>  Fullstack Developer</h4>
+        <h4 className='email'>  arunkumar413@gmail.com</h4>
+        <h4 className='phone'>  +919966288507</h4>
+        <hr />
+        <div className='skills'>
+          <div>
+            <h3> Nodejs </h3>
+          </div>
+          <div>
+            <h3> HTML </h3>
+          </div>
+          <div>
+            <h3> JavaScript </h3>
+          </div>
+          <div>
+            <h3> CSS </h3>
+          </div>
+          <div>
+            <h3> ExpressJs </h3>
+          </div>
+          <div>
+            <h3> Postgres </h3>
+          </div>
+          <div>
+            <h3> MySQL </h3>
+          </div>
+          <div>
+            <h3> Neo4j</h3>
+          </div>
+          <div>
+            <h3> MongoDB </h3>
+          </div>
+          <div>
+            <h3> Rest API </h3>
+          </div>
+          <div>
+            <h3> OpenAPI </h3>
+          </div>
+          <div>
+            <h3> React </h3>
+          </div>
+          <div>
+            <h3> Rust </h3>
+          </div>
 
+        </div>
 
 
       </div>
 
-      <div className='third-column skills'>
-        <div>
-          <h3> Nodejs </h3>
-        </div>
-        <div>
-          <h3> HTML </h3>
-        </div>
-        <div>
-          <h3> JavaScript </h3>
-        </div>
 
-        <div>
-          <h3> CSS </h3>
-        </div>
 
-        <div>
-          <h3> ExpressJs </h3>
-        </div>
 
-        <div>
-          <h3> Postgres </h3>
-        </div>
-        <div>
-          <h3> MySQL </h3>
-        </div>
 
+      <div className='third-column experience'>
+        <h2> Work history </h2>
 
       </div>
-
-
-
     </div >
   );
 }
